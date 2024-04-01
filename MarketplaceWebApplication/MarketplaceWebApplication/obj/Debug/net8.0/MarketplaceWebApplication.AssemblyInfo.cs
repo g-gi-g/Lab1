83cc2fe3cf0d2e691f8a8c295113114c56312764
Yehor_Yelenin <eelenin568@gmail.com>
2024-04-01 23:15:15 +0300

    Дизайн

--- MarketplaceWebApplication/MarketplaceWebApplication/obj/Debug/net8.0/MarketplaceWebApplication.AssemblyInfo.cs
+++ MarketplaceWebApplication/MarketplaceWebApplication/obj/Debug/net8.0/MarketplaceWebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketplaceWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc918ba7439f12586f2a408b546fd6b86e2abb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b4f266ba024cfdec7c7477cdbdfaffc9fdd654c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketplaceWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketplaceWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
