32c621fbb5f9f4fb7fc2616d9b32fdb673a72e2b
Yehor_Yelenin <eelenin568@gmail.com>
2024-04-14 20:07:06 +0300

    1.7
    Трохи почистив файли

--- MarketplaceWebApplication/MarketplaceWebApplication/obj/Debug/net8.0/MarketplaceWebApplication.AssemblyInfo.cs
+++ MarketplaceWebApplication/MarketplaceWebApplication/obj/Debug/net8.0/MarketplaceWebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketplaceWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcfbab9a5ed36027932b87c0e1d08a960b0803cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109850f548d96f0f9b5bf0cf2efebd5a87ac1028")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketplaceWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketplaceWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
